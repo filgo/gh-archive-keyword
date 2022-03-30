0125e82737c1fdae8c2c73b99e82f435f0ddf47b
filgo <philippe.ferreira@gmail.com>
2022-03-30 17:41:32 +0200

    add import events command

--- src/Command/ImportGitHubEventsCommand.php
+++ src/Command/ImportGitHubEventsCommand.php
@@ -4,9 +4,17 @@ declare(strict_types=1);
 
 namespace App\Command;
 
+use App\Client\GHArchiveClientInterface;
+use App\Dto\GHArchive\EventOutput;
+use App\Factory\EventFactory;
+use App\Repository\ReadEventRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * This command must import GitHub events.
@@ -16,16 +24,62 @@ class ImportGitHubEventsCommand extends Command
 {
     protected static $defaultName = 'app:import-github-events';
 
+    private GHArchiveClientInterface $gHArchiveClient;
+    private ReadEventRepository $readEventRepository;
+    private ValidatorInterface $validator;
+    private EntityManagerInterface $em;
+    private EventFactory $eventFactory;
+
+    public function __construct(
+        GHArchiveClientInterface $gHArchiveClient,
+        ReadEventRepository $readEventRepository,
+        ValidatorInterface $validator,
+        EntityManagerInterface $em,
+        EventFactory $eventFactory
+    ) {
+        $this->gHArchiveClient = $gHArchiveClient;
+        $this->readEventRepository = $readEventRepository;
+        $this->validator = $validator;
+        $this->em = $em;
+        $this->eventFactory = $eventFactory;
+
+        parent::__construct();
+    }
+
     protected function configure(): void
     {
         $this
-            ->setDescription('Import GH events');
+            ->setDescription('Import GH events')
+            ->addArgument('import_date', InputArgument::REQUIRED, 'Date of import ? (Example 2022-01-01)')
+        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        // Let's rock !
-        // It's up to you now
+        $io = new SymfonyStyle($input, $output);
+        $io->title('Import GH Archive');
+
+        $this->em->getConnection()->getConfiguration()->setSQLLogger(null);
+
+        foreach ($io->progressIterate(range(0, 23)) as $hour) {
+            $enventsOutput = $this->gHArchiveClient->retrieveData($input->getArgument('import_date').'-'.$hour);
+
+            if (null !== $enventsOutput) {
+                /** @var EventOutput $eventOutput */
+                foreach ($enventsOutput as $eventOutput) {
+                    $errors = $this->validator->validate($eventOutput);
+
+                    if (0 === \count($errors) && false === $this->readEventRepository->exist($eventOutput->id)) {
+                        $event = $this->eventFactory->createFromGHArchiveEventType($eventOutput);
+                        $this->em->persist($event);
+                        $this->em->flush();
+                        $this->em->clear();
+                    }
+                }
+            }
+        }
+
+        $io->progressFinish();
 
         return 1;
     }
